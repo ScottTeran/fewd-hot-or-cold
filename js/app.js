e65cd5b8565ad45599ddf82090147cfe996f9b3f
Scott Teran <wayteran@gmail.com>
2015-06-04 13:42:29 -0600

    notes added

--- js/app.js
+++ js/app.js
@@ -24,12 +24,29 @@ $(document).ready(function(){
 
 });
 
+// NEW GAME
+/* When the page loads, JavaScript 
+should start a new game. Since you’ll 
+need to be able to start a new game 
+when the user clicks the “New Game” 
+button, you’ll want to create a 
+newGame function that does everything 
+necessary to start a new game. */
+
+function newGame() {
+  // first, run the secret number function and assign the value to secretNum
+  secretNum = getSecretNum();
+  feedback = "Make Your Guess!";
+  
+
+
+} 
+
 function guess() {
     // I'm gonn
   var guess = $("userGuess").val();
     // 
     if (!isValidGuess(guess)){
-      // note to self: what is the point of userGuess?
       userGuess = "";
       updateDisplay();
       return;
@@ -44,8 +61,16 @@ function guess() {
 
 
 
-	// secret number
-  function secretNum (){
+	// SECRET NUMBER
+  /* When a new game starts, a secret 
+  number between 1 and 100 should be 
+  generated that the user will have to 
+  guess. You should write a named 
+  function that takes care of this. 
+  You should try to start a new game 
+  without refreshing or reloading the 
+  page. */
+  function getSecretNum() {
     return Math.floor((Math.random() * 100) + 1);
   }
 	
